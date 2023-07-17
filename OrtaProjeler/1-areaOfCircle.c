013a30a3ab83d7ec1bb4b953e259c177445512f5
Ilkay Beydah Saglam <beydahsaglam@hotmail.com>
2023-07-18 00:24:05 +0300

    Update and rename 1-SifreUretici.c to 1-areaOfCircle.c

---
+++ OrtaProjeler/1-areaOfCircle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define PI 3.14159
+
+//Level 3: A program that calculates the area and circumference of a circle.
+
+int main()
+{
+    double radius, area, circumference;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%lf", &radius);
+
+    area = PI * radius * radius;
+    circumference = 2 * PI * radius;
+
+    printf("Area of the circle: %.2lf\n", area);
+    printf("Circumference of the circle: %.2lf\n", circumference);
+
+    return 0;
+}
